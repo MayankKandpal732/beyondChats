fbfdec999858118bcfb90de470df3a7c870a6405
mayank <kandpalm04@gmail.com>
2025-12-30 16:33:07 +0530

    Prepare backend for deployment

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.get("/", (req, res) => {
 
 app.use("/articles", articleRoutes);
 
-app.listen(5000, () => {
-  console.log("Server started : port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
